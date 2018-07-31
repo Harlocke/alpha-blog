43c36237c85b8fbbc1a3760e766729c0c2f624cf
harkaz <harkaz@gmail.com>
2018-07-31 19:45:33 +0000

    se adiciona estilo a los mensajes flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
     @article=Article.new(article_params)
     
     if @article.save
-      flash[:notice]="Articulo Creado"
+      flash[:success]="Articulo Creado"
       redirect_to article_path(@article)
     else
       render 'new'
@@ -27,7 +27,7 @@ class ArticlesController < ApplicationController
   def update
     
     if @article.update(article_params)
-      flash[:notice]="El articulo se ha actualizado"
+      flash[:success]="El articulo se ha actualizado"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -41,7 +41,7 @@ class ArticlesController < ApplicationController
   def destroy
     
     @article.destroy
-    flash[:notice] ="El articulo se eliminó exitosamente"
+    flash[:danger] ="El articulo se eliminó exitosamente"
     redirect_to articles_path
   end
   
