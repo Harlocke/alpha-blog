76e3750a061377f665e117392201c5cff1c306b7
harkaz <harkaz@gmail.com>
2018-08-02 00:18:13 +0000

    seed 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user =User.create([{username:"Harlynton"},{email:"harkaz@gmail.com"},{password:"ironhill"}])
+User.create(username:"Harlynton",email:"harkaz@gmail.com",password:"ironhill")
