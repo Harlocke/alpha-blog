9a27f7eaaaaecdaf0cf4011821efafe13c89b5a9
harkaz <harkaz@gmail.com>
2018-08-08 17:08:57 +0000

    Adicionada funcionalidad de usuario admin

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -56,7 +56,7 @@ class ArticlesController < ApplicationController
     end  
     
     def require_same_user
-      if current_user !=@article.user
+      if current_user !=@article.user and !current_user.admin?
         flash[:danger] ="Solo puedes modificar tus propios artículos!"
         redirect_to root_path
       end
